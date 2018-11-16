31d69e2e13f624c4dd49fbbb0944968e7e9fb14e
Casey Watts <casey.s.watts@gmail.com>
2018-11-16 09:50:01 -0500

    update user-list-test to use modern qunit style

--- tests/integration/components/user-list-test.js
+++ tests/integration/components/user-list-test.js
@@ -3,11 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('GravatarImageComponent', function(hooks) {
+module('UsersListComponent', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('renders', async function() {
-    await render(hbs`{{gravatar-image}}`);
-    assert.ok(this.element.querySelector('img'));
+  test('renders', async function(assert) {
+    await render(hbs`{{user-list}}`);
+    assert.ok(2 == 2);
   });
 });
