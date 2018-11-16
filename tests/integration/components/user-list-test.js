9f527526e52b2d78e7cefc4628c2ce6349567d7e
Casey Watts <casey.s.watts@gmail.com>
2018-11-16 10:06:10 -0500

    start mirage in component test based on https://www.ember-cli-mirage.com/docs/v0.4.x/manually-starting-mirage/

--- tests/integration/components/user-list-test.js
+++ tests/integration/components/user-list-test.js
@@ -2,9 +2,18 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { startMirage } from '../../../initializers/ember-cli-mirage';
 
 module('UsersListComponent', function(hooks) {
   setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.server = startMirage();
+  }),
+
+  hooks.afterEach(function() {
+    this.server.shutdown();
+  }),
 
   test('renders', async function(assert) {
     const users = ['a', 'b'];
